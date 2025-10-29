b36f9b0ee9ac978c0c10de523e971df2a8e84290
kiran pangeni <kiranpangeni43@gmail.com>
2025-10-29 12:54:25 +0545

    product-detals

--- userpage/urls.py
+++ userpage/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home_page,name="home-page"),
+    path('product/<int:pk>/',views.product_detail, name="product-detail")
 ]
--- userpage/views.py
+++ userpage/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from product.models import Product
 
 def home_page(request):
-    return render(request,"userpage/homepage.html")
+    products = Product.objects.all()[:6]
+    return render(request,"userpage/homepage.html",{
+        'products': products
+    })
+
+
+def product_detail(request, pk):
+    products=Product.objects.get(pk=pk)
+    return render(request,"userpage/product_detail.html",{
+        'product':products
+    })
